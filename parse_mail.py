23d153f59a57b752d1addf3b163db0a402f4c1d3
Leo <forever3580ya@gmail.com>
2017-10-28 16:40:05 +0800

    Add url detections and translation

--- parse_mail.py
+++ parse_mail.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-# requiremt: munpack (apt), googletrans (pip3)
+# requiremt: munpack (apt), googletrans (pip3), urlextract (pip3)
 
 import sys
 import os
 import re
 import subprocess
+import requests
 import bs4
 import googletrans
+import urlextract
 
 
 def gen_parsed_file(filename):
     # extract html file and plain content
     try:
-        cmd = "munpack -t -f " + filename
+        cmd = "munpack " + filename + " -t"
         ret = subprocess.check_output(cmd.split())
     except subprocess.CalledProcessError:
         print(cmd + "is failed")
@@ -31,6 +33,11 @@ def read_file(filename):
     return lines
         
 
+def write_file(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
+
+
 def get_eml_content(filename):
     gen_parsed_file(filename)
 
@@ -41,21 +48,43 @@ def get_eml_content(filename):
 
 
 def get_url(eml_content):
-    # TODO: regular expression cannot cover all urls
-    regex1 = "http[s]?://[a-zA-Z0-9\./_]+"
-    regex2 = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    extractor = urlextract.URLExtract()
 
-    urls = re.findall(regex1, eml_content[0])
-    urls += re.findall(regex2, eml_content[0])
-    urls += re.findall(regex1, eml_content[1])
-    urls += re.findall(regex2, eml_content[1])
+    url_part1 = extractor.find_urls(eml_content[0])
+    url_part2 = extractor.find_urls(eml_content[1])
+
+    print(url_part1 + url_part2)
+    return url_part1 + url_part2
 
 
+def get_ret_type(html_content):
+    pretar_str = '<div class="labeltitlesmallresult">'
+    tar_addr = html_content.find(pretar_str) + len(pretar_str)
+    ret_type = html_content[tar_addr, tar_addr + 16].split("</div>")[0]
+    return ret_type
+
+
+def detect_url_type(url_list):
+    base_url = "https://global.sitesafety.trendmicro.com"
+    target_url = "https://global.sitesafety.trendmicro.com/result.php"
+
+    ret_type = []
+
+    for url in url_list:
+        payload = {"urlname": url}
+        with requests.session() as session:
+            session.get(base_url)
+            res = session.post(target_url, data=payload)
+
+        ret_type = get_ret_type(res.text)
+            
+        
 def translate_content(eml_content):
-    # TODO: if part of words are english, googletrans cannot work correctly
     translator = googletrans.Translator()
     content0 = translator.translate(eml_content[0]).text
     content1 = translator.translate(eml_content[1]).text
+
+    return [content0, content1]
 
 
 def clear_generated_files():
@@ -75,14 +104,18 @@ def main():
     eml_content = get_eml_content(sys.argv[1]) 
 
     # retrieve url
-    get_url(eml_content)
+    url_list = get_url(eml_content)
 
-    # TODO: send url to phishing detect web site
+    # send url to trend site safety center to help us detect url type 
+    detect_url_type(url_list)
 
-    #  translate to English
-    translate_content(eml_content)
+    # translate to English
+    trans_content = translate_content(eml_content)
 
     # TODO: send part1 and part2 to testing model
+    dst_path = "./text_file/"
+    for context in trans_content:
+        write_file(dst_path + sys.argv[1][:-4] + ".txt", context)
 
     clear_generated_files()
 
